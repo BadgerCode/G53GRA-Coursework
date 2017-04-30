ade802525c2db5121c2bb90d626a9e3697946951
Michael Hawkins <BadgerCode@users.noreply.github.com>
2017-04-30 15:41:07 +0100

    Updates start position to be inside the house

--- G53GRA-Coursework/Framework/Utility/Camera.cpp
+++ G53GRA-Coursework/Framework/Utility/Camera.cpp
@@ -17,10 +17,10 @@ Camera::Camera() : wKey(0), sKey(0), aKey(0), dKey(0), currentButton(0), mouseX(
 }
 
 void Camera::Reset(){
-	// set the camera position to start at (0,10,0)
+	// set the camera position to start at (0,110,0)
 	eyePosition[0] = 0.0f;
 	eyePosition[1] = 110.0f;
-	eyePosition[2] = 0.5f * static_cast<float>(Scene::GetWindowHeight()) / static_cast<float>(tan(M_PI / 6.0));//0.0f;
+	eyePosition[2] = 0.f;
 
 	// set the view direction vector of the camera to be (0,0,-1)
 	vd[0] = 0.0f;
