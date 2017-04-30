35eea8c0f69df99a9b5a5e26f0c10cadb3a91de9
Michael Hawkins <BadgerCode@users.noreply.github.com>
2017-04-30 07:21:01 +0100

    Adds debug way to disable lighting

--- G53GRA-Coursework/Code/Map.cpp
+++ G53GRA-Coursework/Code/Map.cpp
@@ -5,11 +5,11 @@ Map::Map()
 	_mapSize = 10000;
 	_skybox = new Skybox(_mapSize * 2, "./Textures/skybox.bmp");
 
-	auto lightLevel = -0.35f; // -0.35
+	_lightLevel = -0.35f;
 
 	_sunlight = new Light(GL_LIGHT0, 2500.f, 2500.f, -2500.f);
 	_sunlight->SetAsGlobalLight(true);
-	_sunlight->SetAmbience(lightLevel, lightLevel, lightLevel, 1.f);
+	_sunlight->SetAmbience(_lightLevel, _lightLevel, _lightLevel, 1.f);
 	_sunlight->SetDiffuse(0.f, 0.f, 0.f, 1.f);
 	_sunlight->SetSpecular(0.f, 0.f, 0.f, 1.f);
 
@@ -22,6 +22,23 @@ void Map::Display()
 	_sunlight->Display();
 
 	RenderFloor();
+}
+
+void Map::HandleKey(unsigned char key, int state, int x, int y)
+{
+	if(key == 'f' && state == 0)
+	{
+		if(_debugEnableLighting)
+		{
+			_debugEnableLighting = false;
+			_sunlight->SetAmbience(1, 1, 1, 1.f);
+		}
+		else
+		{
+			_debugEnableLighting = true;
+			_sunlight->SetAmbience(_lightLevel, _lightLevel, _lightLevel, 1.f);
+		}
+	}
 }
 
 void Map::RenderFloor()
