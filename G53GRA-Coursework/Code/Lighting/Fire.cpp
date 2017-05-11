4b5e02af9c47b14b73e2d88e224fe79f8bc46385
Michael Hawkins <BadgerCode@users.noreply.github.com>
2017-05-11 05:09:20 +0100

    Adds embers

--- G53GRA-Coursework/Code/Lighting/Fire.cpp
+++ G53GRA-Coursework/Code/Lighting/Fire.cpp
@@ -1,18 +1,35 @@
 #include "Fire.h"
 
 const double Fire::EmberRadius = 0.75;
-const float Fire::EmberSpeed = 3.0;
+const float Fire::EmberVerticalSpeed = 20.0f;
+const float Fire::EmberHorizontalSpeed = 10.0f;
+const float Fire::MaxEmberHeight = 50.0f;
 
 Fire::Fire(float x, float y, float z): Light(x, y, z)
 {
 	_emberPositions.push_back(new float[3]{ pos[0], pos[1], pos[2] + 10.f });
+	_emberPositions.push_back(new float[3]{ pos[0] - 17.5f, pos[1] + 50.5f, pos[2] + 10.f });
+	_emberPositions.push_back(new float[3]{ pos[0] + 15.f, pos[1] + 35.f, pos[2] + 10.f });
+	_elapsedTime = 0.f;
+
+	_maxEmberY = pos[1] + MaxEmberHeight;
 }
 
 void Fire::Update(const double& deltaTime)
 {
+	_elapsedTime += deltaTime;
+
+	auto offset = sin(_elapsedTime * EmberHorizontalSpeed) / 100;
+
 	for (auto emberPosition : _emberPositions)
 	{
-		emberPosition[1] += static_cast<float>(deltaTime) * EmberSpeed;
+		emberPosition[0] += offset;
+		emberPosition[1] += static_cast<float>(deltaTime) * EmberVerticalSpeed;
+
+		if(emberPosition[1] > _maxEmberY)
+		{
+			emberPosition[1] -= floor(emberPosition[1] / _maxEmberY) * _maxEmberY;
+		}
 	}
 }
 
--- G53GRA-Coursework/Code/Lighting/Fire.h
+++ G53GRA-Coursework/Code/Lighting/Fire.h
@@ -9,8 +9,13 @@ public:
 	void Update(const double& deltaTime) override;
 	void Display() override;
 private:
-	static const double EmberRadius;
-	static const float EmberSpeed;
+	double _elapsedTime;
 
+	static const double EmberRadius;
+	static const float EmberVerticalSpeed;
+	static const float EmberHorizontalSpeed;
+	static const float MaxEmberHeight;
+
+	float _maxEmberY;
 	std::vector<float*> _emberPositions;
 };
