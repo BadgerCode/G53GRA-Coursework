7a852b020e160f3da5fe7060b4d1150e600f02e4
Michael Hawkins <BadgerCode@users.noreply.github.com>
2017-05-11 04:03:11 +0100

    Attepts to smoothen out collisions. Max collision cube walls don't work and smoothing in positive directions doesn't work

--- G53GRA-Coursework/Code/StaticObjectCollisionManager.cpp
+++ G53GRA-Coursework/Code/StaticObjectCollisionManager.cpp
@@ -7,11 +7,18 @@ void StaticObjectCollisionManager::AddCollisionCube(std::vector<float> minPos, s
 	_collisionCubes.push_back(new CollisionCube(minPos, maxPos));
 }
 
-float* StaticObjectCollisionManager::RestrainMovement(float currentPos[], float direction[], float size[])
+float* StaticObjectCollisionManager::RestrainMovement(float currentPos[], float velocity[], float size[])
 {
-	float x = currentPos[0] + direction[0];
-	float y = currentPos[1] + direction[1];
-	float z = currentPos[2] + direction[2];
+	bool cancelVelocity[3] = { false, false, false };
+	int direction[3] = { 
+		velocity[0] < 0 ? -1 : 1,
+		velocity[1] < 0 ? -1 : 1,
+		velocity[2] < 0 ? -1 : 1,
+	};
+
+	float x = currentPos[0] + velocity[0];
+	float y = currentPos[1] + velocity[1];
+	float z = currentPos[2] + velocity[2];
 
 	float minX = x - size[0] / 2;
 	float maxX = x + size[0] / 2;
@@ -26,8 +33,21 @@ float* StaticObjectCollisionManager::RestrainMovement(float currentPos[], float 
 			&& (minY <= cube->maxY && maxY >= cube->minY)
 			&& (minZ <= cube->maxZ && maxZ >= cube->minZ))
 		{
-			return currentPos;
+			int reverseDirection[3] = {
+				(maxX - cube->maxX) < 0 ? -1 : 1,
+				(maxY - cube->maxY) < 0 ? -1 : 1,
+				(maxZ - cube->maxZ) < 0 ? -1 : 1,
+			};
+
+			cancelVelocity[0] = direction[0] != reverseDirection[0] || cancelVelocity[0];
+			cancelVelocity[1] = direction[1] != reverseDirection[1] || cancelVelocity[1];
+			cancelVelocity[2] = direction[2] != reverseDirection[2] || cancelVelocity[2];
 		}
 	}
+
+	if (cancelVelocity[0]) x -= velocity[0];
+	if (cancelVelocity[1]) y -= velocity[1];
+	if (cancelVelocity[2]) z -= velocity[2];
+
 	return new float[3]{ x, y, z};
 }
